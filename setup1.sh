c19d4cc8243d50922f02f1c3c3c6b25ba4cfc15b
Tony363 <thefool363@gmail.com>
2024-06-25 02:23:30 -0400

    run this this weekend

--- setup1.sh
+++ setup1.sh
@@ -1,3 +1,6 @@
 conda remove --name rppg-toolbox --all -y
 conda create -n rppg-toolbox -y #python=3.8 pytorch torchvision torchaudio pytorch-cuda -c pytorch -c nvidia
-conda install pytorch torchvision==0.14.0 torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+conda activate rppg-toolbox
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121i
+
+#conda install pytorch torchvision==0.14.0 torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
